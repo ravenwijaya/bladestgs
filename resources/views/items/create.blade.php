a5622c44e3c578dfcdbfece5b1e3d420c6471e6b
ravenwijaya <58552967+ravenwijaya@users.noreply.github.com>
2020-07-01 18:47:34 +0700

    first commit

---
+++ resources/views/items/create.blade.php
@@ -0,0 +1,12 @@
+@extends('adminlte.master')
+@section('content')
+<h1> ini hal create</h1>
+@endsection
+
+@push('scripts')
+<script>
+    var wrappers =document.getElementsByClassName("wrappers");
+   var items=["ini", "contoh"];
+console.log("ini items:",items)
+</script>
+@endpush
---
+++ resources/views/items/index.blade.php
@@ -0,0 +1,4 @@
+@extends('adminlte.master')
+@section('content')
+<h1> halo</h1>
+@endsection
